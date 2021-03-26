66f4f644833284b67b9dce5653af06ab44e2f4be
HieuNguyen <hieunguyen@MacBook-Air-cua-HieuNguyen.local>
2021-03-26 20:49:07 +0700

    Update ALLOWED_HOSTS with site and development server URL

--- thucvat/settings.py
+++ thucvat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&&jbm@6o6rjb#)d$%ofv8g38)ob6nh
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [kthuocviet.herokuapp.com]
 
 
 # Application definition
