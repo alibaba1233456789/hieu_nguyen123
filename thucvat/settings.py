a98ee32d5908799ba8b9a7fb78ed52ec51bf7060
HieuNguyen <hieunguyen@MacBook-Air-cua-HieuNguyen.local>
2021-03-26 20:15:03 +0700

    Added files and changes required for deployment to heroku

--- thucvat/settings.py
+++ thucvat/settings.py
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=2000)
 DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
