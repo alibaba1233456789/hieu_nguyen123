0c49b21c8df1acfa5872fe7341b5c5535eb07abb
HieuNguyen <hieunguyen@MacBook-Air-cua-HieuNguyen.local>
2021-03-29 17:01:22 +0700

    Added files and c

--- thucvat/settings.py
+++ thucvat/settings.py
@@ -151,7 +151,6 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
